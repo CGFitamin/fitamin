03270b8bbfe6c52d14714fdd98df0067518fa36e
Choi-Jungwoo <jungwoo@cineuse.com>
2022-07-17 20:22:06 +0800

    feat: impl docker linux development environment

--- src/fitamin/__main__.py
+++ src/fitamin/__main__.py
@@ -1,12 +1,4 @@
 import uvicorn
-from fastapi import FastAPI
-
-app = FastAPI(title='Fitamin')
-
-
-def main():
-    uvicorn.run('__main__:app', host='0.0.0.0', port=8000, reload=True)
-
 
 if __name__ == '__main__':
-    main()
+    uvicorn.run('fitamin.server:app', host='0.0.0.0', port=8000, reload=True)
